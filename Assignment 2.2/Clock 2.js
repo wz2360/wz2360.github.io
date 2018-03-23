2e0299fab4be6747afc2a6fbead7728d7e4fd372
wz2360 <wenjingzhang2015@gmail.com>
2018-03-23 12:03:59 -0400

    snow

--- Assignment 2.2/Clock 2.js
+++ Assignment 2.2/Clock 2.js
@@ -1,49 +1,82 @@
-var hour_xs = [], hour_ys = [];
-var minute_xs = [], minute_ys = [];
-var second_xs = [], second_ys = [];
+var xs=[], ys=[];
+var x1=[], y1=[];
+var x2=[], y2=[];
 
-function setup(){
-  createCanvas(600,600);
-  frameRate(1); //1 FPS, since nothing needs to change between seconds
+function setup() {
+  createCanvas(600, 600);
   noStroke();
-
-  // Set the random positions for this session (so balls merely show/hide,
-  // they don't actually move in this clock.)
-  for (var i = 0; i < 24; i++){
-    append(hour_xs, random(width));
-    append(hour_ys, random(height));
+  for (var i = 0; i <60; i++) {
+    append(xs, random(width));
+    append(ys, random(height));
   }
-
-  for (var i = 0; i < 60; i++){
-    append(minute_xs, random(width));
-    append(minute_ys, random(height));
+   for (var i = 0; i <200; i++) {
+    append(x1, random(width));
+    append(y1, random(height));
   }
-
-  for (var i = 0; i < 60; i++){
-    append(second_xs, random(width));
-    append(second_ys, random(height));
+  for (var i = 0; i<200; i++) {
+    append(x2, random(width));
+    append(y2, random(height));
   }
 }
 
-function draw(){
-  background(255);
-  fill(0);
 
-  for (var i = 0; i < second(); i++){
-    drawBall(second_xs[i], second_ys[i], 10, 190);
+function draw() {
+  var h = hour();
+  var m = minute();
+  var s = second();
+
+  if(h == 12 || h == 0) {
+    h = 12;
+  }
+  else {
+    h = h%12;
   }
 
-  for (var i = 0; i < minute(); i++){
-    drawBall(minute_xs[i], minute_ys[i], 40, 150);
-  }
 
-  for (var i = 0; i < hour(); i++){
-    drawBall(hour_xs[i], hour_ys[i], 90, 80);
-  }
+  background(250, 250, 250, 100);
 
+  push();
+  for (var i = 0; i < 200; i++) {
+    fill(255);
+    ellipse(x1[i], y1[i], 1, 1);
+  }
+  pop();
+
+  push();
+  for (var i = 0; i < s; i++) {
+    drawSnow(xs[i], ys[i], 1, 20, 7);
+  }
+  pop();
+
+  
+  push();
+  fill(218, 237, 247, 100 );
+  rect(0, 0, 600, 600-map(minute(), 0, 60, 0, 600));
+  pop();
+
+  push();
+  textSize(40);
+  textFont('Helvetica')
+  fill(192,192,192, 240);
+  text('the current time is: ' + h + ':' + m + ':' + s, 0, 570);
+  pop();
 }
 
-function drawBall(x, y, dia, k) {
-  fill(k, 150); // grayscale with 150/255 transparency
-  ellipse(x, y, dia, dia);
+
+function drawSnow(x, y, rad1, rad2, npoints) {
+  fill(64, 164, 224);
+  var angle = TWO_PI/npoints;
+  var halfAngle = angle/2.0;
+  beginShape();
+  for(var a=0; a<TWO_PI; a+=angle) {
+    var sx = x + cos(a) * rad2;
+    var sy = y + sin(a) * rad2;
+    vertex(sx, sy);
+    sx = x + cos(a+halfAngle) * rad1;
+    sy = y + sin(a+halfAngle) * rad1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
 }
+
+
--- Assignment 2.2/Clock 3.js
+++ Assignment 2.2/Clock 3.js
@@ -1,49 +1,97 @@
-var hour_xs = [], hour_ys = [];
-var minute_xs = [], minute_ys = [];
-var second_xs = [], second_ys = [];
+var sec_xs=[], sec_ys=[], sec_angles=[];
+var min_xs=[], min_ys=[], min_angles=[];
+var second_colors = ['rgba(81,149,72,1)', 'rgba(136,196,37,1)', 'rgba(190,242,2,1)'];
+var minute_colors = ['rgba(204,97,71,1)', 'rgba(240,159,39,1)', '#CC724C'];
+var bkgrd_color = 'rgba(27,103,107,1)';
 
-function setup(){
-  createCanvas(600,600);
-  frameRate(1); //1 FPS, since nothing needs to change between seconds
-  noStroke();
+function setup() {
+  createCanvas(640, 640);
+  background(bkgrd_color);
+  textSize(16);
+  textAlign(CENTER);
+  fill('rgba(190,242,2,0.75)');
+  text("loading...", 320, 320);
 
-  // Set the random positions for this session (so balls merely show/hide,
-  // they don't actually move in this clock.)
-  for (var i = 0; i < 24; i++){
-    append(hour_xs, random(width));
-    append(hour_ys, random(height));
-  }
+  frameRate(1);            // 1 FPS
+  stroke(bkgrd_color);
+  angleMode(DEGREES);
+  textAlign(LEFT);
 
-  for (var i = 0; i < 60; i++){
-    append(minute_xs, random(width));
-    append(minute_ys, random(height));
-  }
-
-  for (var i = 0; i < 60; i++){
-    append(second_xs, random(width));
-    append(second_ys, random(height));
+  //populate arrays for flower, leaf coordinates
+  for (var i = 0; i < 60; i++) {
+    sec_xs[i] = random(width);
+    sec_ys[i] = random(height);
+    sec_angles[i] = random(360);
+    min_xs[i] = random(width);
+    min_ys[i] = random(height);
+    min_angles[i] = random(360);
   }
 }
 
-function draw(){
-  background(255);
-  fill(0);
+function draw() {
+  background(bkgrd_color);
 
-  for (var i = 0; i < second(); i++){
-    drawBall(second_xs[i], second_ys[i], 10, 190);
+  for (var i = 0; i < second(); i++) {
+    drawSeconds(sec_xs[i], sec_ys[i], sec_angles[i], i%second_colors.length);
   }
 
-  for (var i = 0; i < minute(); i++){
-    drawBall(minute_xs[i], minute_ys[i], 40, 150);
+  for (var i = 0; i < minute(); i++) {
+    drawMinutes(min_xs[i], min_ys[i], min_angles[i], hour(), i%minute_colors.length);
   }
 
-  for (var i = 0; i < hour(); i++){
-    drawBall(hour_xs[i], hour_ys[i], 90, 80);
-  }
-
+  showTime();
 }
 
-function drawBall(x, y, dia, k) {
-  fill(k, 150); // grayscale with 150/255 transparency
-  ellipse(x, y, dia, dia);
+function drawSeconds(x, y, angle, color) {
+  push();
+  translate(x, y);
+  rotate(angle);
+  stroke(bkgrd_color);
+  fill(second_colors[color]);
+  ellipse(0, 0, 13, 26);
+  line(0,-4,0,13);
+  stroke(second_colors[color]);
+  line(0,12,0,18);
+  pop();
+}
+
+function drawMinutes(x, y, angle, hour, color) {
+  push();
+  fill(minute_colors[color]);
+  translate(x, y);
+  rotate(angle);
+  for (var i = 0; i < hour; i++) {
+    push();
+    rotate(360*i/hour);
+    ellipse(0, 13, 26*pow(0.92, hour), 26);
+    pop();
+  }
+  fill('#E9D558');
+  ellipse(0,0,10,10);
+  pop();
+}
+
+function testFlowers() {
+  for (var i = 0; i < 24; i++) {
+    drawFlower(50+100*(i%6), 50+100*(i/6), i+1, i%flower_colors.length);
+  }
+}
+
+function twoDigits(number) {
+  return ("0" + number).slice(-2);
+}
+
+function showTime() {
+  rectMode(CORNER);
+  fill('rgba(27,103,107,0.5)');
+  rect(20, 570, 70, 50);
+
+  var time = '';
+  time += twoDigits(hour()) + (hour() == 1 ? ' hour\n' : ' hour\n');
+  time += twoDigits(minute()) + (minute() == 1 ? ' minute\n' : ' minute\n');
+  time += twoDigits(second()) + (second() == 1 ? ' second\n' : ' second');
+
+  textSize(12);
+  fill('rgba(190,242,2,0.75)');
+  text(time, 25, 585);
 }
